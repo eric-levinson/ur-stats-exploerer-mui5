e22d7431694509beaf25fc36e625f71f6c21abed
Eric <eric.levinson42@gmail.com>
2021-09-25 17:54:48 -0500

    prep for prod

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 //import pages
-import HomePage from './pages/HomePage';
+
 import {ClippedDrawer} from './common/containers/ClippedDrawer'
 
 class Routes extends React.Component {
@@ -12,8 +12,8 @@ class Routes extends React.Component {
     render() {
       return (
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/explore' component={ClippedDrawer} origin='season-10-j1nooa6jlw' />
+          <Redirect exact from="/" to="/explore" />
+          <Route path='/explore' component={ClippedDrawer} />
           <Route
           render={function() {
             return <h1>Not Found</h1>;
