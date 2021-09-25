0ec635d9baf243ad15b6117db95baf1c22d68644
Eric <eric.levinson42@gmail.com>
2021-09-25 17:46:31 -0500

    oh boy

--- src/common/components/data/OnlyTable.jsx
+++ src/common/components/data/OnlyTable.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import DataGrid from 'react-data-grid';
 
 import ThingsContext from './ThingContext'
@@ -6,18 +6,18 @@ import ThingsContext from './ThingContext'
 export const SimpleTable = props => {
 
     const things = useContext(ThingsContext)
-    
-    
+
+    let columns = things.[0].columns
+    let rows = things.[0].rows
 
     return (
-       <> {console.log(things.[0].columns)}
-          {console.log(things.[0].rows)}
-                   <div style={{ display: 'flex', height: '100%', width: '100%' }}>
+        <>
+            <div style={{ display: 'flex', height: '100%', width: '100%' }}>
                 <div style={{ flexGrow: 1, }}>
                     <DataGrid
-                        columns={things.[0].columns}
-                        rows={things.[0].rows} 
-
+                        style={{ height: '85vh' }}
+                        columns={columns}
+                        rows={rows}
                     />
                 </div>
             </div></>
